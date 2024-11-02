fe78d5f85403c4f4d3148b015ed30d4d4acc8d81
PleaseNoHomework <adsadsf1@gmail.com>
2024-11-02 11:14:56 +0900

    add wave

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int enemyNo;
+    public int speed;
+    public int HP;
+}
---
+++ Assets/Scripts/manager/WaveSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public List<GameObject> enemy;
+    // 각각의 프리팹을 적 유형별로 설정
+    public GameObject itemPrefab;             // 아이템 프리팹
+    public Vector3 spawnPoint;
+    private float time;
+    
+    void spawnEnemy(Vector3 spawnPoint, int enemyNo)
+    {
+        Instantiate(enemy[enemyNo], spawnPoint, Quaternion.identity);
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= 2)
+        {
+            spawnEnemy(spawnPoint, 0);
+            time = 0;
+            Debug.Log("Spawn!");
+        }
+    }
+}
