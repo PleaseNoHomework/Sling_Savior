f26fc6e8f4b829583cf4e2bd39360119eb3f8f86
cunalcun <cunalcun@naver.com>
2024-12-04 23:37:48 +0900

    asdf
    asdf

--- Assets/Scripts/manager/PauseManager.cs
+++ Assets/Scripts/manager/PauseManager.cs
@@ -1,46 +1,34 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class PauseMenu : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
-    public GameObject pauseMenuUI;
+    public static bool GameIsPaused = false; // 현재 게임 상태
+    public GameObject pauseMenuUI; // 일시정지 메뉴 UI
+    public GameObject pauseButton;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
-    }
-
+    // 게임 재개 메서드
     public void Resume()
     {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
+        pauseMenuUI.SetActive(false); // Pause UI 비활성화
+        Time.timeScale = 1f; // 게임 진행 재개
+        GameIsPaused = false; // 게임이 진행 상태로 변경
     }
 
-    void Pause()
+    // 게임 일시정지 메서드
+    public void Pause()
     {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPaused = true;
+        pauseMenuUI.SetActive(true); // Pause UI 활성화
+        Time.timeScale = 0f; // 게임 멈춤
+        GameIsPaused = true; // 게임이 일시정지 상태로 변경
     }
 
+    // 게임 종료 메서드
     public void ExitGame()
     {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+        UnityEditor.EditorApplication.isPlaying = false; // 에디터에서 종료
 #else
-        Application.Quit();
+        Application.Quit(); // 빌드 환경에서 종료
 #endif
     }
 }
